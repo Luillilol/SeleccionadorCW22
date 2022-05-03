6fadc86ee9420d4a7b8d858f242fb3c7dcb28026
Luillilol <luilli359@gmail.com>
2022-05-03 15:30:38 -0500

    Actualización ruleta

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 window.onload= function(){
+    let body = document.getElementById('body');
     let nombreFinal = document.querySelector('.pNombre');
+    let imgSus = document.getElementById("imgSus");
     const audio = new Audio('./sus.mp3');
     let alumnos = ["Ricardo Sánchez", "Ana Karen", "Antonio Sebas", "Mia Constanza", "Andrés Rojas", "Andrea Barrios", "José Morales", "Edgar Jesús", "Christian Isaac", "Jenny Torres",
                     "Regina Lara", "Yadid Sebastián", "Zadig Alberto", "Mariana López", "Ana Martinez", "Santiago Pedro", "Gian Marco", "Frida Sofía", "José Antonio", "María José", "Gabriel Eduardo",
@@ -9,6 +11,18 @@ window.onload= function(){
     console.log(alumnos);
     let boton = document.getElementById("buscar");
     let nombrePantalla = document.getElementById("p_nombre");
+
+
+    boton.addEventListener("mouseover", ()=>{
+        console.log("OVER");
+        imgSus.style.opacity = '100';
+    })
+    boton.addEventListener("mouseleave", ()=>{
+        console.log("LEAVE");
+        imgSus.style.opacity = '0';
+    });
+
+
     boton.addEventListener("click", ()=>{
         audio.play();
         
@@ -35,8 +49,7 @@ window.onload= function(){
                 setTimeout(()=>{
                     nombrePantalla.innerHTML = alumnos[numOriginal];
                     console.log("Hola");
-                    nombreFinal.classList.add('animate__animated', 'animate__rubberBand');
-
+                    nombreFinal.classList.add('animate__animated', 'animate__tada');                    
                 }, 6200);
             })
            
@@ -47,6 +60,11 @@ window.onload= function(){
         console.log(numOriginal);
         console.log(alumnos[numOriginal]);
         nombrePantalla.innerHTML = alumnos[numOriginal];
+        nombreFinal.classList.remove('animate__animated', 'animate__tada');
+
+
+        // body.style = ;
+
         // console.log(alumnos[num_random]);
     })
 }
